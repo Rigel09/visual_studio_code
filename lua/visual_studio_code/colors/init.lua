9850ee5b2136421bf509cd4c06b813cff92960f5
Rigel09 <joshua.newton102@gmail.com>
2023-11-12 12:01:48 -0600

    Starting adding support for dark modern and light modern

--- lua/visual_studio_code/colors/init.lua
+++ lua/visual_studio_code/colors/init.lua
@@ -1,14 +1,16 @@
 local M = {
-    dark = require("visual_studio_code.colors.dark"),
-    light = require("visual_studio_code.colors.light"),
+	dark = require("visual_studio_code.colors.dark"),
+	light = require("visual_studio_code.colors.light"),
+	dark_modern = require("visual_studio_code.colors.dark_modern"),
+	light_modern = require("visual_studio_code.colors.light_modern"),
 }
 
 function M.get_colors(mode)
-    if not vim.tbl_contains(vim.tbl_keys(M), mode) then
-        return M["dark"]
-    end
+	if not vim.tbl_contains(vim.tbl_keys(M), mode) then
+		return M["dark"]
+	end
 
-    return M[mode]
+	return M[mode]
 end
 
 return M
